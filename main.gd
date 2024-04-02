6b2ed76a8076fc4cf41eaefd49e6f04288e3acc7
Seth Louis <sethvihanlouis@gmail.com>
2024-04-02 15:01:50 +0700

    added streak counter

--- main.gd
+++ main.gd
@@ -28,23 +28,25 @@ func _input(event):
 
 
 func update_labels():
+	if stats_res.goal_amount > 0:
+		%GoalAmount.text = str(stats_res.goal_amount)
+	else:
+		%GoalAmount.text = ""
+	%GoalUnits.text = stats_res.goal_units
+	%GoalName.text = stats_res.goal_name
+	if stats_res.total_amount_done > 0:
+		%TotalDoneAmount.text = str(stats_res.total_amount_done)
+	else:
+		%TotalDoneAmount.text = ""
+	%UnchangingUnitsLabel.text = stats_res.goal_units + "."
+	%UnchangingUnitsLabel2.text = stats_res.goal_units + "."
+	if stats_res.goal_amount > stats_res.total_amount_done:
+		%AmountLeftLabel.text = "I have " + str(stats_res.goal_amount - stats_res.total_amount_done) + " " + stats_res.goal_units + " left."
+	else:
 		if stats_res.goal_amount > 0:
-			%GoalAmount.text = str(stats_res.goal_amount)
-		else:
-			%GoalAmount.text = ""
-		%GoalUnits.text = stats_res.goal_units
-		%GoalName.text = stats_res.goal_name
-		if stats_res.total_amount_done > 0:
-			%TotalDoneAmount.text = str(stats_res.total_amount_done)
-		else:
-			%TotalDoneAmount.text = ""
-		%UnchangingUnitsLabel.text = stats_res.goal_units + "."
-		%UnchangingUnitsLabel2.text = stats_res.goal_units + "."
-		if stats_res.goal_amount > stats_res.total_amount_done:
-			%AmountLeftLabel.text = "I have " + str(stats_res.goal_amount - stats_res.total_amount_done) + " " + stats_res.goal_units + " left."
-		else:
-			if stats_res.goal_amount > 0:
-				%AmountLeftLabel.text = "Great job! You did it!"
+			%AmountLeftLabel.text = "Great job! You did it!"
+
+	$Control/VBoxContainer/Control2/StreakLabel.text = "Streak: " + str(stats_res.current_streak)
 
 
 func _on_goal_amount_text_submitted(new_text):
@@ -81,7 +83,7 @@ func _on_today_done_amount_text_submitted(new_text):
 	if float(new_text) > 0:
 		$bonus.play()
 	stats_res.total_amount_done += float(new_text)
-	check_for_streak(stats_res.last_entry_daystamp)
+	check_for_streak()
 	ResourceSaver.save(stats_res, save_file_path + save_file_name)
 	update_labels()
 	%TodayDoneAmount.text = ""
@@ -116,29 +118,34 @@ func _update_all_stats_resource():
 	update_labels()
 
 
-func check_for_streak(days_since_epoch):
+func check_for_streak():
+	var current_days_since_epoch = get_current_days_since_epoch()
 	# first we rule out that another entry is being made on the same day
-	if days_since_epoch == stats_res.last_entry_daystamp:
+	if current_days_since_epoch == stats_res.last_entry_daystamp:
 		return
 	# if the current day is one day after the last entry then we add one to our counter
-	if days_since_epoch == stats_res.last_entry_daystamp + 1:
+	if current_days_since_epoch == stats_res.last_entry_daystamp + 1:
 		stats_res.current_streak += 1
 	
 	else:
 		# reset streak
 		stats_res.current_streak = 0
-	ResourceSaver.save(stats_res, save_file_path + save_file_name)
 	
 	# create a timestamp overriding the current timestamp
-	create_timestamp()
+	stats_res.last_entry_daystamp = current_days_since_epoch
+	
+	ResourceSaver.save(stats_res, save_file_path + save_file_name)
 
 
-func create_timestamp():
+func get_current_days_since_epoch():
 	# get current system time in sytem's time zone
+	var current_datetime_dict = Time.get_datetime_dict_from_system(false)
+	
+	# convert this to unix time
+	var seconds_since_epoch = Time.get_unix_time_from_datetime_dict(current_datetime_dict)
 	# convert this time to days since unix epoch
-	# save that overriding current var in stats resource and then save that to computer
-	pass
-
+	var days_since_epoch = seconds_since_epoch / 86400
+	return days_since_epoch
 
 func _on_close_button_pressed():
 	_update_all_stats_resource()
--- stats_resource.gd
+++ stats_resource.gd
@@ -6,7 +6,7 @@ class_name StatsResource
 @export var goal_name: String
 @export var total_amount_done: float
 
-@export var current_streak: int
+@export var current_streak: int = 0
 # this will be the timestamp of the last entry
 # the timestamp will be how many days it's been since the unix epoch
-@export var last_entry_daystamp: int
+@export var last_entry_daystamp: int = 19814
